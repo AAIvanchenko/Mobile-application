2878eb06436ab49e349951e34fbc57988f46e546
Anastasia Ivanchenko <89zazaza89@gmail.com>
2022-05-17 15:01:25 +0700

    Практическая 4
    Работа с картой. Добавление меток на карте
    Работа с открыт API. Погода в Красноярске

--- app/src/main/java/com/example/corgiworld/MapActivity.kt
+++ app/src/main/java/com/example/corgiworld/MapActivity.kt
@@ -1,23 +1,55 @@
 package com.example.corgiworld
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
-import androidx.appcompat.widget.Toolbar
-import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import kotlinx.android.synthetic.main.activity_map.*
 
-class MapActivity : AppCompatActivity() {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private lateinit var mHeader: AccountHeader
+    private val KRASNOYARS = LatLng( 56.01528, 92.89325)
+    private val MINUSINSK = LatLng( 56.003100, 92.799517)
+    private val DUDINKA = LatLng(56.002625, 92.764402)
+    private val SKVER = LatLng(56.012095, 92.814739)
 
+
+    private var markerPerth: Marker? = null
+    private var markerSydney: Marker? = null
+    private var markerBrisbane: Marker? = null
+
+    private lateinit var mMap: GoogleMap
+    private  lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         var mToolbar: Toolbar = findViewById(R.id.maptoolbar)
 
         setSupportActionBar(mToolbar)
@@ -94,5 +126,62 @@ class MapActivity : AppCompatActivity() {
     fun weatherShow(){
         val weatherIntent = Intent(this, WeatherActivity::class.java)
         startActivity(weatherIntent)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        mMap = map
+        val cameraPosition = CameraPosition.Builder()
+            .target(KRASNOYARS) // Sets the center of the map to Mountain View
+            .zoom(12f)            // Sets the zoom
+            .bearing(90f)         // Sets the orientation of the camera to east
+            .tilt(30f)            // Sets the tilt of the camera to 30 degrees
+            .build()              // Creates a CameraPosition from the builder
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        markerPerth = map.addMarker(
+            MarkerOptions()
+                .position(SKVER)
+                .title("Сквер имени степанова")
+                .snippet("Скверпарк культуры и отдыха")
+        )
+        markerPerth?.tag = 0
+        markerSydney = map.addMarker(
+            MarkerOptions()
+                .position(MINUSINSK)
+                .title("Парк Юнатов")
+                .snippet("Можно погулять с собакой")
+        )
+        markerSydney?.tag = 0
+        markerBrisbane = map.addMarker(
+            MarkerOptions()
+                .position(DUDINKA)
+                .title("Гремячая грива")
+                .snippet("Может быть много клещей")
+        )
+        markerBrisbane?.tag = 0
+
+        // Set a listener for marker click.
+        map.setOnMarkerClickListener(this)
+    }
+
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        // Retrieve the data from the marker.
+        val clickCount = marker.tag as? Int
+
+        // Check if a click count was set, then display the click count.
+        clickCount?.let {
+            val newClickCount = it + 1
+            marker.tag = newClickCount
+            Toast.makeText(
+                this,
+                "${marker.title}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false
     }
 }
--- app/src/main/java/com/example/corgiworld/WeatherActivity.kt
+++ app/src/main/java/com/example/corgiworld/WeatherActivity.kt
@@ -1,9 +1,12 @@
 package com.example.corgiworld
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
@@ -11,12 +14,19 @@ import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class WeatherActivity : AppCompatActivity() {
+    val CITY: String = "Krasnoyarsk"
+    val API: String = "06c921750b9a82d8f5d1294e1586276f" // Use API key
     private lateinit var mHeader: AccountHeader
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+        weatherTask().execute()
 
         var mToolbar: Toolbar = findViewById(R.id.weather_toolbar)
 
@@ -72,6 +82,64 @@ class WeatherActivity : AppCompatActivity() {
                     }
                     return false
                 }}).build()
+
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        @Deprecated("Deprecated in Java")
+        override fun onPreExecute() {
+            super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
+            findViewById<TextView>(R.id.errorText).visibility = View.GONE
+        }
+
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response:String?
+
+            try{
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
+                    Charsets.UTF_8
+                )
+            }catch (e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                /* Extracting JSON returns from the API */
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getInt("temp").toString()+"°C"
+                val weatherDescription = weather.getString("description")
+
+                val address = jsonObj.getString("name")+", "+sys.getString("country")
+
+                /* Populating extracted data into our views */
+                findViewById<TextView>(R.id.address).text = address
+                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
+                findViewById<TextView>(R.id.temp).text = temp
+                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
+
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
+
+            } catch (e: Exception) {
+                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+            }
+
+    }
 
     }
 
